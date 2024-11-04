2826a16d534f0460bf4659b3eb2ec779ec40348a
AnthonyR <anthony156gamer@gmail.com>
2024-11-04 20:15:59 +0100

    feat: add dtos and update entities

--- src/main/java/com/picit/iam/dto/UserDto.java
+++ src/main/java/com/picit/iam/dto/UserDto.java
@@ -1,8 +1,19 @@
 package com.picit.iam.dto;
 
+import com.picit.iam.model.Settings;
 import lombok.Builder;
 
 @Builder
 public record UserDto(
+        String id,
+        String username,
+        String email,
+        String profilePicture,
+        String bio,
+        String[] hobbies,
+        String[] follows,
+        Settings settings,
+        String createdAt,
+        String updatedAt
 ) {
 }
--- src/main/java/com/picit/iam/model/User.java
+++ src/main/java/com/picit/iam/model/User.java
@@ -1,11 +1,39 @@
 package com.picit.iam.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document(collection = "user")
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String profilePicture;
+
+    private String bio;
+
+    private String[] hobbies;
+
+    private String[] follows;
+
+    private Settings settings;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
