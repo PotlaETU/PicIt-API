bc9cc9278f9f2c79bee4620caeb742d86bf45473
antoine <antoine_goudaillier@ens.univ-artois.fr>
2024-11-05 17:05:54 +0100

    ajout de l'entity message

--- src/main/java/com/picit/message/entity/Message.java
+++ src/main/java/com/picit/message/entity/Message.java
@@ -1,4 +1,17 @@
 package com.picit.message.entity;
 
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "messages")
 public class Message {
+    @Id
+    private String id;
+    private String sender_id;
+    private String room_id;
+    private String content;
+    private LocalDateTime createdAt;
 }
