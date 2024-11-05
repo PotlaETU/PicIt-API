8ae27ac8bca5ac96474b57e994a35609065dd527
antoine <antoine_goudaillier@ens.univ-artois.fr>
2024-11-05 13:32:19 +0100

    ajout des dto pour les posts et commentaires

--- src/main/java/com/picit/post/dto/PostDto.java
+++ src/main/java/com/picit/post/dto/PostDto.java
@@ -1,4 +1,22 @@
 package com.picit.post.dto;
 
-public class PostDto {
+import com.picit.post.entity.Comment;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record PostDto (
+    UUID id,
+    String username,
+    String content,
+    String photoUrl,
+    List<UUID> likes,
+    List<Comment> comments,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+) {
+
 }
