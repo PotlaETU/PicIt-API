b374fd7f42a4027b054e01b2411ff08d34ee8452
antoine <antoine_goudaillier@ens.univ-artois.fr>
2024-11-05 13:33:25 +0100

    ajout du mapper pour les posts

--- src/main/java/com/picit/post/mapper/PostMapper.java
+++ src/main/java/com/picit/post/mapper/PostMapper.java
@@ -1,4 +1,14 @@
 package com.picit.post.mapper;
 
+import com.picit.post.dto.PostDto;
+import com.picit.post.model.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface PostMapper {
+    PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
+
+    Post postDtoToPost(PostDto postDto);;
+    PostDto postToPostDto(Post post);
 }
