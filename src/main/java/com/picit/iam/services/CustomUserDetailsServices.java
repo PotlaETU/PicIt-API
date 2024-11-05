76a05ed6244b9708044599d6c4dea7d2d919fb6f
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2024-11-05 14:42:19 +0100

    feat: add token refresh logic to services

--- src/main/java/com/picit/iam/services/CustomUserDetailsServices.java
+++ src/main/java/com/picit/iam/services/CustomUserDetailsServices.java
@@ -1,7 +1,10 @@
 package com.picit.iam.services;
 
+import com.picit.iam.model.User;
 import com.picit.iam.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,6 +18,10 @@ public class CustomUserDetailsServices implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getAuthorities());
     }
 }
--- src/main/java/com/picit/iam/services/IamService.java
+++ src/main/java/com/picit/iam/services/IamService.java
@@ -2,16 +2,27 @@ package com.picit.iam.services;
 
 import com.picit.iam.auth.JwtUtil;
 import com.picit.iam.dto.LoginRequest;
+import com.picit.iam.dto.LoginResponse;
 import com.picit.iam.dto.SignUpRequest;
+import com.picit.iam.dto.TokenRefreshRequest;
 import com.picit.iam.mapper.UserMapper;
+import com.picit.iam.model.Settings;
 import com.picit.iam.model.User;
 import com.picit.iam.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -22,28 +33,108 @@ public class IamService {
     private final UserMapper userMapper;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final Logger logger = LoggerFactory.getLogger(IamService.class);
 
-    public void signUp(SignUpRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.username())) {
-            throw new RuntimeException("Username already exists");
+    public ResponseEntity<LoginResponse> signUp(SignUpRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.username()) || userRepository.existsByEmail(signUpRequest.email())) {
+            throw new UsernameNotFoundException("Username or email already exists");
         }
 
         var user = userMapper.toUser(signUpRequest);
+        user.setRefreshToken(jwtUtil.generateRefreshToken(user));
+        user.setSettings(Settings.builder()
+                .build());
         user.getSettings().setNotifications(signUpRequest.notifications());
         if (signUpRequest.privacy()) {
             user.getSettings().setPrivacy("public");
         } else {
             user.getSettings().setPrivacy("private");
         }
-
-        //TODO: hash password and authenticate user
+        user.setPassword(passwordEncoder.encode(signUpRequest.password()));
         userRepository.save(user);
+        String token = jwtUtil.generateToken(user);
+
+        var loginResponse = LoginResponse.builder()
+                .token(token)
+                .expiration(Date.from(new Date()
+                                .toInstant()
+                                .plusMillis(jwtUtil.getExpirationTime()))
+                        .toString())
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .refreshToken(user.getRefreshToken())
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(loginResponse);
     }
 
-    public String login(LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(
+    public ResponseEntity<LoginResponse> login(LoginRequest loginRequest) {
+        logger.info("Login request received for username: {}", loginRequest.username());
+
+        User authUser = userRepository.findByUsername(loginRequest.username());
+        if (authUser == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password())
         );
-        return jwtUtil.generateToken((User) authentication.getPrincipal());
+        String token = jwtUtil.generateToken(authUser);
+        String refreshToken;
+        if (jwtUtil.isTokenValid(authUser.getRefreshToken(), authUser)) {
+            refreshToken = authUser.getRefreshToken();
+        } else {
+            refreshToken = jwtUtil.generateRefreshToken(authUser);
+            authUser.setRefreshToken(refreshToken);
+            userRepository.save(authUser);
+        }
+
+        var loginResponse = LoginResponse.builder()
+                .token(token)
+                .expiration(Date.from(new Date()
+                                .toInstant()
+                                .plusMillis(jwtUtil.getExpirationTime()))
+                        .toString())
+                .email(authUser.getEmail())
+                .username(authUser.getUsername())
+                .refreshToken(refreshToken)
+                .build();
+        logger.info("Login response generated for username: {}", loginRequest.username());
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    public ResponseEntity<LoginResponse> refresh(TokenRefreshRequest tokenRefreshRequest) {
+        String refreshToken = tokenRefreshRequest.refreshToken();
+        String username = jwtUtil.extractUsername(refreshToken);
+
+        User authUser = userRepository.findByUsername(username);
+        if (jwtUtil.isTokenValid(refreshToken, authUser)) {
+            String newAccessToken = jwtUtil.generateToken(authUser);
+            var loginResponse = LoginResponse.builder()
+                    .token(newAccessToken)
+                    .refreshToken(refreshToken)
+                    .build();
+            return ResponseEntity.ok(loginResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
+
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        String token = authorizationHeader.substring(7);
+        String username = jwtUtil.extractUsername(token);
+
+        User authUser = userRepository.findByUsername(username);
+        if (authUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        authUser.setRefreshToken(null);
+        userRepository.save(authUser);
+        return ResponseEntity.ok().build();
     }
 }
