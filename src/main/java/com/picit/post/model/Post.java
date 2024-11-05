9fcd0f43f46404b77e9a74a6eafdf712ef188042
antoine <antoine_goudaillier@ens.univ-artois.fr>
2024-11-05 13:33:52 +0100

    ajout du model pour les posts

--- src/main/java/com/picit/post/model/Post.java
+++ src/main/java/com/picit/post/model/Post.java
@@ -1,4 +1,20 @@
 package com.picit.post.model;
 
+import com.picit.post.entity.Comment;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 public class Post {
+    private UUID id;
+    private String username;
+    private String content;
+    private String photoUrl;
+    private List<UUID> likes;
+    private List<Comment> comments;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
 }
