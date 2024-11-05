e3205279e6c655b3745566fa3cbbf9f254ef4331
antoine <antoine_goudaillier@ens.univ-artois.fr>
2024-11-05 17:05:34 +0100

    ajout du DTO pour les messages

--- src/main/java/com/picit/message/dto/MessageDto.java
+++ src/main/java/com/picit/message/dto/MessageDto.java
@@ -1,4 +1,16 @@
 package com.picit.message.dto;
 
-public class MessageDto {
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record MessageDto (
+        String id,
+        String sender_id,
+        String room_id,
+        String content,
+        LocalDateTime createdAt
+) {
+
 }
