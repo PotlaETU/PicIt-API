3868477a8b73e26723feca71590bc8b7db61bef1
AnthonyR <anthony156gamer@gmail.com>
2024-11-07 20:32:26 +0100

    feat: add profile pic endpoint

---
+++ src/main/java/com/picit/iam/controller/ProfileController.java
@@ -0,0 +1,32 @@
+package com.picit.iam.controller;
+
+import com.picit.iam.dto.user.UserProfileDto;
+import com.picit.iam.services.UserProfileService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/profile")
+public class ProfileController {
+
+    private final UserProfileService profileService;
+
+    @PostMapping("/picture")
+    public ResponseEntity<String> addOrUpdateProfilePicture(Authentication authentication, @RequestParam("file") MultipartFile file) {
+        return profileService.updateProfilePicture(authentication.getName(), file);
+    }
+
+    @GetMapping("/picture")
+    public ResponseEntity<byte[]> getProfilePicture(Authentication authentication) {
+        return profileService.getProfilePicture(authentication.getName());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createProfile(Authentication authentication, @RequestBody UserProfileDto bio) {
+        return profileService.createProfile(authentication.getName(), bio);
+    }
+}
