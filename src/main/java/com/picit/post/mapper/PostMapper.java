abbb9475d7dc340ac43d04dd00f3358e69820462
antoine <antoine_goudaillier@ens.univ-artois.fr>
2024-11-08 14:39:36 +0100

    adding mapping

--- src/main/java/com/picit/post/mapper/PostMapper.java
+++ src/main/java/com/picit/post/mapper/PostMapper.java
@@ -1,12 +1,23 @@
 package com.picit.post.mapper;
 
 import com.picit.post.dto.PostDto;
+import com.picit.post.dto.PostRequestDto;
 import com.picit.post.entity.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
     Post postDtoToPost(PostDto postDto);
 
     PostDto postToPostDto(Post post);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "likes", ignore = true)
+    @Mapping(target = "comments", ignore = true)
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())")
+    Post updatePostFromPostRequestDto(PostRequestDto postRequestDto, @MappingTarget Post post);
 }
