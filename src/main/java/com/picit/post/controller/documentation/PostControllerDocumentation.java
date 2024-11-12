75f9905ea3736101e775a559fb3b846f71afb720
AnthonyR <anthony156gamer@gmail.com>
2024-11-12 22:08:45 +0100

    refacto: cleanup code

--- src/main/java/com/picit/post/controller/documentation/PostControllerDocumentation.java
+++ src/main/java/com/picit/post/controller/documentation/PostControllerDocumentation.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
