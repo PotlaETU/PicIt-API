cd9530668b8bfe38703f8d0fa5de357e21d4f86f
Rolland Anthony <anthony_rolland@ens.univ-artois.fr>
2024-11-13 13:23:30 +0000

    Likes and Comments

---
+++ src/main/java/com/picit/post/controller/CommentController.java
@@ -0,0 +1,34 @@
+package com.picit.post.controller;
+
+import com.picit.post.controller.documentation.CommentControllerDocumentation;
+import com.picit.post.dto.CommentDto;
+import com.picit.post.dto.comment.CommentRequestDto;
+import com.picit.post.services.CommentService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/comment")
+public class CommentController implements CommentControllerDocumentation {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public CommentDto createComment(Authentication authentication, @Valid @RequestBody CommentRequestDto commentRequestDto) {
+        return commentService.createComment(authentication.getName(), commentRequestDto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteComment(Authentication authentication, String commentId) {
+        return commentService.deleteComment(authentication.getName(), commentId);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateComment(Authentication authentication, @Valid @RequestBody CommentRequestDto commentRequestDto) {
+        return commentService.updateComment(authentication.getName(), commentRequestDto);
+    }
+}
