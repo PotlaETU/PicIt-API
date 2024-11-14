83247268cec29705b84718fc7eebda7c907315e1
AnthonyR <anthony156gamer@gmail.com>
2024-11-14 18:39:53 +0100

    feat: add faker datas to db when api starts (10 users and 10 posts)

--- src/main/java/com/picit/mongoutils/MongoFakerDatas.java
+++ src/main/java/com/picit/mongoutils/MongoFakerDatas.java
@@ -2,17 +2,36 @@ package com.picit.mongoutils;
 
 import com.picit.iam.entity.Settings;
 import com.picit.iam.entity.User;
+import com.picit.iam.entity.UserProfile;
+import com.picit.iam.repository.UserRepository;
 import com.picit.post.entity.Hobby;
 import com.picit.post.entity.Post;
+import com.picit.post.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import net.datafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Locale;
 
 @Service
+@RequiredArgsConstructor
 public class MongoFakerDatas {
-    Faker faker = new Faker(Locale.FRANCE);
+
+    private final UserRepository userRepository;
+
+    private final PostRepository postRepository;
+
+    private final Faker faker = this.faker();
+
+    @Bean
+    public Faker faker() {
+        return new Faker(Locale.FRANCE);
+    }
 
     private String getFakerUsername() {
         return faker.internet().username();
@@ -27,7 +46,7 @@ public class MongoFakerDatas {
     }
 
     private String getFakerContent() {
-        return faker.lorem().characters(10, 20);
+        return faker.lorem().paragraph(3);
     }
 
     private String getFakerTitle() {
@@ -39,6 +58,14 @@ public class MongoFakerDatas {
                 .email(getFakerEmail())
                 .username(getFakerUsername())
                 .password(getFakerPassword())
+                .role("FAKE_USER")
+                .userProfile(UserProfile.builder()
+                        .followers(List.of(faker.internet().uuid(), faker.internet().uuid(), faker.internet().uuid(), faker.internet().uuid()))
+                        .username(getFakerUsername())
+                        .bio(faker.lorem().characters(10, 20))
+                        .build())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
                 .settings(Settings.builder()
                         .notifications(true)
                         .privacy("public")
@@ -51,8 +78,23 @@ public class MongoFakerDatas {
                 .updatedAt(LocalDateTime.now())
                 .createdAt(LocalDateTime.now())
                 .content(getFakerContent())
+                .isPublic(true)
                 .hobby(Hobby.DETENTE)
                 .userId("1")
                 .build();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            for (int i = 0; i < 10; i++) {
+                User user = this.buildFakeUser();
+                userRepository.save(user);
+            }
+            for (int i = 0; i < 10; i++) {
+                Post post = this.buildFakePost();
+                postRepository.save(post);
+            }
+        };
     }
 }
