89c6144084e40345b0b51db799a4f35683f9bfa7
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2024-12-16 13:56:44 +0100

    refacto: useless import

--- src/main/java/com/picit/post/controller/documentation/PostControllerDocumentation.java
+++ src/main/java/com/picit/post/controller/documentation/PostControllerDocumentation.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 
