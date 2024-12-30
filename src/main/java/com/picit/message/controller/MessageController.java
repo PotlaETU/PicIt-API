93f5702c99cd65f989dc8613160be7021a9e6286
AnthonyR <anthony156gamer@gmail.com>
2024-12-30 16:46:45 +0100

    feat: update controller method response

--- src/main/java/com/picit/message/controller/MessageController.java
+++ src/main/java/com/picit/message/controller/MessageController.java
@@ -2,8 +2,10 @@ package com.picit.message.controller;
 
 import com.picit.message.controller.documentation.MessageControllerDocumentation;
 import com.picit.message.dto.MessageDto;
+import com.picit.message.dto.MessageResponseDto;
 import com.picit.message.services.MessageService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -11,20 +13,20 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class MessageController implements MessageControllerDocumentation {
 
     private final MessageService messageService;
 
-    @MessageMapping("chat.sendMessage")
-    @SendTo("/topic/chat/{roomId}")
-    public MessageDto sendMessageToConvId(@Payload MessageDto chatMessage,
-                                          SimpMessageHeaderAccessor headerAccessor,
-                                          @DestinationVariable("roomId") String conversationId) {
-        messageService.sendMessageToConvId(chatMessage, conversationId, headerAccessor);
-        return chatMessage;
+    @MessageMapping("/chat/{roomId}")
+    @SendTo("/topic/messages/{roomId}")
+    public MessageResponseDto sendMessageToConvId(@Payload MessageDto chatMessage,
+                                                  SimpMessageHeaderAccessor headerAccessor,
+                                                  @DestinationVariable String roomId) {
+        log.info("Received message: {} from {} in room {}", chatMessage.content(), chatMessage.senderUsername(), roomId);
+        return messageService.sendMessageToConvId(chatMessage, roomId, headerAccessor);
     }
-
 
 }
